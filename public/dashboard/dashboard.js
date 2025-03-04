5be103922eb7a4f7fe05decd78546e69995ca81f
Sudeera <89583016+HashanSudeera@users.noreply.github.com>
2025-03-04 15:08:34 +0530

    ss

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -29,3 +29,43 @@ function closeAllSubMenus() {
         ul.previousElementSibling.classList.remove('rotate')
     })
 }
+
+//fuel sensor data 
+
+function updateFuelLevel(level) {
+    const water = document.getElementById("waterLevel");
+    const text = document.getElementById("fuelText");
+    water.style.height = level + "%";
+    text.innerText = level + "%";
+}
+// Example: Simulating data update
+setTimeout(() => updateFuelLevel(75), 1000); // Smoothly fill to 100% in 5 seconds
+
+
+//battery sensor data
+
+const charge = document.querySelector(".charge");
+const batteryPercentage = document.getElementById("battery-percentage");
+const batterySlider = document.getElementById("battery-slider");
+
+function updateBattery(level) {
+    charge.style.height = level + "%";
+
+    if (level <= 25) {
+        charge.style.background = "var(--red)";
+    } else if (level <= 50) {
+        charge.style.background = "var(--orange)";
+    } else if (level <= 75) {
+        charge.style.background = "var(--yellow)";
+    } else if (level <= 100) {
+        charge.style.background = "var(--green)";
+    }
+
+    batteryPercentage.innerText = `Battery: ${level}%`;
+}
+
+batterySlider.addEventListener("input", (event) => {
+    updateBattery(event.target.value);
+});
+
+updateBattery(0);
